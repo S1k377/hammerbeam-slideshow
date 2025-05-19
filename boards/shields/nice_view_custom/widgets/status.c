03a2a24b76f5074aed368f4fc9d205de9eae99cf
S1k377 <sikemetalcurse@gmail.com>
2025-05-19 22:59:53 +0200

    Update status.c

--- boards/shields/nice_view_custom/widgets/status.c
+++ boards/shields/nice_view_custom/widgets/status.c
@@ -48,10 +48,6 @@ LV_IMG_DECLARE(hammerbeam28);
 LV_IMG_DECLARE(hammerbeam29);
 LV_IMG_DECLARE(hammerbeam30);
 
-
-
-static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
-
 const lv_img_dsc_t *anim_imgs[] =  {
     &hammerbeam1,
     &hammerbeam2,
