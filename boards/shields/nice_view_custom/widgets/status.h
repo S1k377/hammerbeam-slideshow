8aed1dd7cfd725cd060b7eb1d19125facb758946
S1k377 <sikemetalcurse@gmail.com>
2025-05-20 01:01:22 +0200

    Update status.h

--- boards/shields/nice_view_custom/widgets/status.h
+++ boards/shields/nice_view_custom/widgets/status.h
@@ -16,11 +16,6 @@ struct zmk_widget_status {
     lv_obj_t *obj;
     lv_color_t cbuf[CANVAS_SIZE * CANVAS_SIZE];
     struct status_state state;
-
-    lv_obj_t *art;
-    struct k_work img_update_work;
-    struct k_timer slideshow_timer;
-    uint32_t next_img_idx;
 };
 
 int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent);
